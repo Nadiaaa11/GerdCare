d06dd42a60b563bb4f3146b75a1e768fdfea30c3
helggaa <119507174+helggaa@users.noreply.github.com>
2023-12-09 15:36:46 +0700

    ambulancePage
    awas kalo error

---
+++ AmbulanceActivity.kt
@@ -0,0 +1,93 @@
+package com.dicoding.ambulance
+
+
+import android.graphics.PorterDuff
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import androidx.databinding.DataBindingUtil
+import com.dicoding.ambulance.databinding.ActivityAmbulanceBinding
+import com.google.android.material.tabs.TabLayout
+import androidx.core.content.ContextCompat
+import android.content.Intent
+import android.widget.FrameLayout
+
+
+
+class MainActivity : AppCompatActivity() {
+
+    private lateinit var tabLayout: TabLayout
+
+    private lateinit var binding: ActivityAmbulanceBinding
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_ambulance)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_ambulance)
+        initUI()
+        ferguso()
+        setupFrameLayoutClickListeners()
+    }
+    private fun setupFrameLayoutClickListeners() {
+        val rmSatu: FrameLayout = findViewById(R.id.rm_satu)
+        val rmDua: FrameLayout = findViewById(R.id.rm_dua)
+
+        rmSatu.setOnClickListener {
+            // Start the new activity for FrameLayout rm_satu
+            startActivity(Intent(this, KlickRs::class.java))
+        }
+
+//        rmDua.setOnClickListener {
+//            // Start the new activity for FrameLayout rm_dua
+//            startActivity(Intent(this, AnotherNewActivity::class.java))
+//        }
+    }
+    private fun initUI(){
+        binding.menuAtas.backButton.setOnClickListener{
+            Toast.makeText(this, "Back button clicked",Toast.LENGTH_SHORT).show()
+        }
+        binding.menuAtas.titikTiga.setOnClickListener{
+            Toast.makeText(this, "More",Toast.LENGTH_SHORT).show()
+        }
+        binding.menuAtas.titleAmb.text = "Ambulance Service"
+    }
+    private fun ferguso() {
+        val tabLayout: TabLayout = findViewById(R.id.menu_bawah)
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.record), 0)
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.home), 1)
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.profile), 2)
+
+        tabLayout.getTabAt(0)?.icon?.setColorFilter(
+            ContextCompat.getColor(this, R.color.pink),
+            PorterDuff.Mode.SRC_IN
+        )
+        tabLayout.getTabAt(1)?.icon?.setColorFilter(
+            ContextCompat.getColor(this, R.color.gray),
+            PorterDuff.Mode.SRC_IN
+        )
+        tabLayout.getTabAt(2)?.icon?.setColorFilter(
+            ContextCompat.getColor(this, R.color.gray),
+            PorterDuff.Mode.SRC_IN
+        )
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                tab.icon?.setColorFilter(
+                    ContextCompat.getColor(this@MainActivity, R.color.pink),
+                    PorterDuff.Mode.SRC_IN
+                )
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                tab.icon?.setColorFilter(
+                    ContextCompat.getColor(this@MainActivity, R.color.gray),
+                    PorterDuff.Mode.SRC_IN
+                )
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+                // Do nothing or handle the reselection as needed
+            }
+        })
+    }
+
+}
---
+++ KlickRs.kt
@@ -0,0 +1,88 @@
+package com.dicoding.ambulance
+
+import android.graphics.PorterDuff
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.widget.Toast
+import android.view.MenuItem
+import android.view.MenuInflater
+import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.dicoding.ambulance.databinding.ActivityMainBinding
+import com.google.android.material.tabs.TabLayout
+import androidx.core.content.ContextCompat
+import android.content.Intent
+import android.widget.ImageView
+import android.widget.FrameLayout
+import android.view.View
+
+
+
+class KlickRs : AppCompatActivity() {
+    private lateinit var helpImageView: ImageView
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_klick_rs)
+        ferguso()
+
+        val backButton1: ImageView = findViewById(R.id.backButton1)
+        val callAmbulanceTextView: TextView = findViewById(R.id.callAmbulance)
+        val btncallambulane: FrameLayout = findViewById(R.id.btncallambulane)
+        helpImageView = findViewById(R.id.helpImageView)
+        btncallambulane.setOnClickListener {
+            // Change the text when btncallambulane is clicked
+            callAmbulanceTextView.text = "Help is on the way..."
+            helpImageView.visibility = View.VISIBLE
+
+            // Add any additional logic or actions you want to perform when the button is clicked
+        }
+
+        // Set an OnClickListener for the backButton1
+        backButton1.setOnClickListener {
+            // Start the new activity when backButton1 is clicked
+//            startActivity(Intent(this, MainActivity::class.java))
+            finish()
+        }
+    }
+
+    private fun ferguso() {
+        val tabLayout: TabLayout = findViewById(R.id.menu_bawah_actdua)
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.record), 0)
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.home), 1)
+        tabLayout.addTab(tabLayout.newTab().setIcon(R.drawable.profile), 2)
+
+        tabLayout.getTabAt(0)?.icon?.setColorFilter(
+            ContextCompat.getColor(this, R.color.pink),
+            PorterDuff.Mode.SRC_IN
+        )
+        tabLayout.getTabAt(1)?.icon?.setColorFilter(
+            ContextCompat.getColor(this, R.color.gray),
+            PorterDuff.Mode.SRC_IN
+        )
+        tabLayout.getTabAt(2)?.icon?.setColorFilter(
+            ContextCompat.getColor(this, R.color.gray),
+            PorterDuff.Mode.SRC_IN
+        )
+
+        tabLayout.addOnTabSelectedListener(object : TabLayout.OnTabSelectedListener {
+            override fun onTabSelected(tab: TabLayout.Tab) {
+                tab.icon?.setColorFilter(
+                    ContextCompat.getColor(this@KlickRs, R.color.pink),
+                    PorterDuff.Mode.SRC_IN
+                )
+            }
+
+            override fun onTabUnselected(tab: TabLayout.Tab) {
+                tab.icon?.setColorFilter(
+                    ContextCompat.getColor(this@KlickRs, R.color.gray),
+                    PorterDuff.Mode.SRC_IN
+                )
+            }
+
+            override fun onTabReselected(tab: TabLayout.Tab) {
+                // Handle the reselection as needed
+            }
+        })
+    }
+}
